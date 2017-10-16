67bb52c211ab886ee458e6c3692aa1656ab3ecc8
Vyacheslav Krachkov <vskrachkov@gmail.com>
2017-10-16 17:46:57 +0300

    [feat] remove redundant temp file

--- envvars.template.sh
+++
@@ -1,5 +0,0 @@
-export PY_CREOSON_API_URL=http://192.168.0.104:9056/creoson;
-export PY_CREOSON_START_DIR=C:\\\\Program\ Files\\\\PTC;
-export PY_CREOSON_START_FILE=nitro_proe_remote.bat;
-export PY_CREO_RETRIES=0;
-export PY_CREO_WORK_DIR=c:\\\\;
